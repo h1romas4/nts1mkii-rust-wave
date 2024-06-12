a48c02e4e07fcbb8f39e960783d41e07c533e339
h1romas4 <h1romas4@gmail.com>
2024-06-12 21:14:22 +0900

    add osc_dummy

---
+++ components/osc_dummy/src/dummy.rs
@@ -0,0 +1,91 @@
+use logue_sdk_v2rs::*;
+
+pub struct Dummy {
+    runtime_desc: *const unit_runtime_desc_t,
+}
+
+impl Dummy {
+    pub fn new() -> Self {
+        Self {
+            runtime_desc: core::ptr::null(),
+        }
+    }
+
+    pub fn init(&mut self, desc: *const unit_runtime_desc_t) -> i8 {
+        if desc.is_null() {
+            return k_unit_err_undef as i8;
+        }
+
+        let desc = unsafe { desc.as_ref().unwrap() };
+
+        if desc.target != unsafe { unit_header.target } {
+            return k_unit_err_target as i8;
+        }
+
+        if !unit_api_is_compat!(desc.api) {
+            return k_unit_err_api_version as i8;
+        }
+
+        if desc.samplerate != 48000 {
+            return k_unit_err_samplerate as i8;
+        }
+
+        if desc.input_channels != 2 || desc.output_channels != 1 {
+            return k_unit_err_geometry as i8;
+        }
+
+        self.runtime_desc = desc;
+
+        k_unit_err_none as i8
+    }
+
+    pub fn teardown(&mut self) {}
+
+    pub fn reset(&mut self) {}
+
+    pub fn resume(&mut self) {}
+
+    pub fn suspend(&mut self) {}
+
+    pub fn process(&mut self, _input: *const f32, output: *mut f32, frames: u32) {
+        let ctxt = unsafe {
+            (*self.runtime_desc).hooks.runtime_context as *const unit_runtime_osc_context_t
+        };
+
+        let _w0 = unsafe {
+            osc_w0f_for_note((((*ctxt).pitch) >> 8) as u8, (((*ctxt).pitch) & 0xff) as u8)
+        };
+
+        let mut y = output;
+        let y_e = unsafe { y.offset(frames as isize) };
+
+        while y != y_e {
+            unsafe {
+                *y = 0.0_f32;
+                y = y.offset(1);
+            }
+        }
+    }
+
+    pub fn set_parameter(&mut self, _index: u8, _value: i32) {}
+
+    pub fn get_parameter_value(&self, _index: u8) -> i32 {
+        0
+    }
+
+    pub fn get_parameter_str_value(&self, _index: u8, _value: i32) -> *const core::ffi::c_char {
+        core::ptr::null()
+    }
+
+    pub fn note_on(&self, _note: u8, _velo: u8) {}
+
+    pub fn note_off(&self, _note: u8) {}
+
+    pub fn all_note_off(&self) {}
+
+    pub fn pitch_bend(&self, _bend: u8) {}
+
+    pub fn channel_pressure(&self, _press: u8) {}
+
+    pub fn after_touch(&self, _note: u8, _press: u8) {}
+}
---
+++ components/osc_dummy/src/header.rs
@@ -0,0 +1,159 @@
+use logue_sdk_v2rs::*;
+
+///
+/// The sound unit header.
+///
+#[used]
+#[link_section = ".unit_header"]
+#[export_name = "unit_header"]
+pub static UNIT_HEADER: unit_header_t = unit_header_t {
+    // This unit's version: major.minor.patch (major<<16 minor<<8 patch).
+    version: 0x00020000,
+    // Size of this header. Leave as is.
+    header_size: core::mem::size_of::<unit_header_t>() as u32,
+    // Target platform and module pair for this unit
+    target: k_unit_target_nts1_mkii | k_unit_module_osc,
+    // API version for which unit was built. See runtime.h
+    api: k_unit_api_2_0_0,
+    // Developer ID. See https://github.com/korginc/logue-sdk/blob/master/developer_ids.md
+    dev_id: sound_unit_dev_id_string!(b"H1RO"),
+    // ID for this unit. Scoped within the context of a given dev_id.
+    unit_id: 0x050400,
+    // Name for this unit, will be displayed on device
+    name: sound_unit_string!(b"DMMY", 20),
+    // Reserved
+    reserved0: 0,
+    reserved1: 0,
+    // Number of valid parameter descriptors. (max. 10)
+    num_params: 8,
+    // Format:
+    // min, max, center, default, type, frac. bits, frac. mode, <reserved>, name
+    // See common/runtime.h for type enum and unit_param_t structure
+    params: [
+        // Fixed/direct UI parameters
+        // A knob
+        unit_param_t {
+            min: 0,
+            max: 1023,
+            center: 0,
+            init: 0,
+            type_: k_unit_param_type_none as u8,
+            _bitfield_align_1: [0u8; 0],
+            // frac : 4, frac_mode : 1, reserved : 3
+            _bitfield_1: __BindgenBitfieldUnit::new([0b00000000; 1]),
+            name: sound_unit_string!(b"SHPE", 22),
+        },
+        // B knob
+        unit_param_t {
+            min: 0,
+            max: 1023,
+            center: 0,
+            init: 0,
+            type_: k_unit_param_type_none as u8,
+            _bitfield_align_1: [0u8; 0],
+            // frac : 4, frac_mode : 1, reserved : 3
+            _bitfield_1: __BindgenBitfieldUnit::new([0b00000000; 1]),
+            name: sound_unit_string!(b"SUB", 22),
+        },
+        // 8 Edit menu parameters
+        unit_param_t {
+            min: 0,
+            max: 0,
+            center: 0,
+            init: 0,
+            type_: k_unit_param_type_enum as u8,
+            _bitfield_align_1: [0u8; 0],
+            // frac : 4, frac_mode : 1, reserved : 3
+            _bitfield_1: __BindgenBitfieldUnit::new([0b00000000; 1]),
+            name: sound_unit_string!(b"WAVE A", 22),
+        },
+        unit_param_t {
+            min: 0,
+            max: 0,
+            center: 0,
+            init: 0,
+            type_: k_unit_param_type_none as u8,
+            _bitfield_align_1: [0u8; 0],
+            // frac : 4, frac_mode : 1, reserved : 3
+            _bitfield_1: __BindgenBitfieldUnit::new([0b00000000; 1]),
+            name: sound_unit_string!(b"WAVE B", 22),
+        },
+        unit_param_t {
+            min: 0,
+            max: 0,
+            center: 0,
+            init: 0,
+            type_: k_unit_param_type_none as u8,
+            _bitfield_align_1: [0u8; 0],
+            // frac : 4, frac_mode : 1, reserved : 3
+            _bitfield_1: __BindgenBitfieldUnit::new([0b00000000; 1]),
+            name: sound_unit_string!(b"SUB WAVE", 22),
+        },
+        unit_param_t {
+            min: 0,
+            max: 1000,
+            center: 0,
+            init: 0,
+            type_: k_unit_param_type_percent as u8,
+            _bitfield_align_1: [0u8; 0],
+            // frac : 4, frac_mode : 1, reserved : 3
+            _bitfield_1: __BindgenBitfieldUnit::new([0b00010001; 1]),
+            name: sound_unit_string!(b"RING MIX", 22),
+        },
+        unit_param_t {
+            min: 0,
+            max: 1000,
+            center: 0,
+            init: 0,
+            type_: k_unit_param_type_none as u8,
+            _bitfield_align_1: [0u8; 0],
+            // frac : 4, frac_mode : 1, reserved : 3
+            _bitfield_1: __BindgenBitfieldUnit::new([0b00010001; 1]),
+            name: sound_unit_string!(b"BIT CRUSH", 22),
+        },
+        unit_param_t {
+            min: 0,
+            max: 1000,
+            center: 0,
+            init: 250,
+            type_: k_unit_param_type_percent as u8,
+            _bitfield_align_1: [0u8; 0],
+            // frac : 4, frac_mode : 1, reserved : 3
+            _bitfield_1: __BindgenBitfieldUnit::new([0b00010001; 1]),
+            name: sound_unit_string!(b"DRIFT", 22),
+        },
+        unit_param_t {
+            min: 0,
+            max: 0,
+            center: 0,
+            init: 0,
+            type_: k_unit_param_type_none as u8,
+            _bitfield_align_1: [0u8; 0],
+            // frac : 4, frac_mode : 1, reserved : 3
+            _bitfield_1: __BindgenBitfieldUnit::new([0u8; 1]),
+            name: [0x0; 22],
+        },
+        unit_param_t {
+            min: 0,
+            max: 0,
+            center: 0,
+            init: 0,
+            type_: k_unit_param_type_none as u8,
+            _bitfield_align_1: [0u8; 0],
+            // frac : 4, frac_mode : 1, reserved : 3
+            _bitfield_1: __BindgenBitfieldUnit::new([0u8; 1]),
+            name: [0x0; 22],
+        },
+        unit_param_t {
+            min: 0,
+            max: 0,
+            center: 0,
+            init: 0,
+            type_: k_unit_param_type_none as u8,
+            _bitfield_align_1: [0u8; 0],
+            // frac : 4, frac_mode : 1, reserved : 3
+            _bitfield_1: __BindgenBitfieldUnit::new([0u8; 1]),
+            name: [0x0; 22],
+        },
+    ],
+};
